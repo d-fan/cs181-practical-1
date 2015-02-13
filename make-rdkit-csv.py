a1c071046f0ae3957289e848de3d96b663fd7c16
Serguei Balanovich <sbalanovuch@college.harvard.edu>
2015-02-12 20:31:29 -0800

    added gap column to new csv

--- make-rdkit-csv.py
+++ make-rdkit-csv.py
@@ -14,7 +14,7 @@ from sklearn import neighbors
 train_filename = 'train.csv.gz'
 test_filename  = 'test.csv.gz'
 pred_filename  = 'example_mean.csv'
-samples = 500
+samples = 5000
 
 def pred_avg(arr, datum):
     total_pred = 0
@@ -41,7 +41,7 @@ def build_svm(train_data):
     # #############################
     r_clf = Ridge(alpha = 1.0)
     l_clf = Lasso(alpha=0.001)
-    en_clf = ElasticNet(alpha=0.001, l1_ratio = 0.1)
+    en_clf = ElasticNet(alpha=0.0001, l1_ratio = 0.7)
     r_clf.fit(train_data['features'], train_data['gap'])
     # l_clf.fit(train_data['features'], train_data['gap'])
     # en_clf.fit(train_data['features'], train_data['gap'])
@@ -72,73 +72,86 @@ with gzip.open(train_filename, 'r') as train_fh:
         #     print "Finished ", i, "trainings"
         smiles   = row[0]
         # features1 = np.array([float(x) for x in row[1:257]])
-        features2 = np.array(AllChem.GetHashedAtomPairFingerprintAsBitVect(Chem.MolFromSmiles(smiles),2))
+        features2 = np.array(AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smiles),2,nBits=4096))
         gap      = float(row[257])
-        if (i < samples):
-            train_data.append({ 'smiles':   smiles,
-                            'features': features2,
-                            'gap':      gap })
-        else:
-            train_data2['smiles'].append(smiles)
-            train_data2['features'].append(features2)
-            train_data2['gap'].append(gap)  
+        # if (i < samples):
+        train_data.append({ 'smiles':   smiles,
+                        'features': features2,
+                        'gap':      gap })
+        # else:
+            # train_data2['smiles'].append(smiles)
+            # train_data2['features'].append(features2)
+            # train_data2['gap'].append(gap)  
+
+        
 
         if (i > samples*2):
             break 
+    # Write a prediction file.
+    with open('new_rdkit_vector.csv', 'w') as pred_fh:
+
+        # Produce a CSV file.
+        pred_csv = csv.writer(pred_fh, delimiter=',', quotechar='"')
+
+        # Write the header row.
+        pred_csv.writerow(['Smile', 'Vector', 'Gap'])
+
+        for datum in train_data:
+            pred_csv.writerow([datum['smiles'], datum['features'], datum['gap']])
 
 # # Compute the mean of the gaps in the training data.
 # gaps = np.array([datum['gap'] for datum in train_data])
 # mean_gap = np.mean(gaps)
 
-# Load the test file.
-# test_data = []
-# test_data2 = []
-# with gzip.open(test_filename, 'r') as test_fh:
+# # Load the test file.
+# # test_data = []
+# # test_data2 = []
+# # with gzip.open(test_filename, 'r') as test_fh:
 
-#     # Parse it as a CSV file.
-#     test_csv = csv.reader(test_fh, delimiter=',', quotechar='"')
+# #     # Parse it as a CSV file.
+# #     test_csv = csv.reader(test_fh, delimiter=',', quotechar='"')
     
-#     # Skip the header row.
-#     next(test_csv, None)
+# #     # Skip the header row.
+# #     next(test_csv, None)
 
-#     # Load the data.
-#     for i, row in enumerate(test_csv):
-#         if (i > samples):
-#             break
-#         # if (i % 10 == 0):
-#         #     print "Finished ", i, "tests"
-#         id       = row[0]
-#         smiles   = row[1]
-#         features = np.array([float(x) for x in row[2:258]])
+# #     # Load the data.
+# #     for i, row in enumerate(test_csv):
+# #         if (i > samples):
+# #             break
+# #         # if (i % 10 == 0):
+# #         #     print "Finished ", i, "tests"
+# #         id       = row[0]
+# #         smiles   = row[1]
+# #         features = np.array([float(x) for x in row[2:258]])
         
-#         test_data.append({ 'id':       id,
-#                            'smiles':   smiles,
-#                            'features': features })
-        # test_data2['id'].append(id)
-        # test_data2['smiles'].append(smiles)
-        # test_data2['features'].append(features)
+# #         test_data.append({ 'id':       id,
+# #                            'smiles':   smiles,
+# #                            'features': features })
+#         # test_data2['id'].append(id)
+#         # test_data2['smiles'].append(smiles)
+#         # test_data2['features'].append(features)
 
-# Write a prediction file.
-with open(pred_filename, 'w') as pred_fh:
+# # Write a prediction file.
+# with open(pred_filename, 'w') as pred_fh:
 
-    # Produce a CSV file.
-    pred_csv = csv.writer(pred_fh, delimiter=',', quotechar='"')
+#     # Produce a CSV file.
+#     pred_csv = csv.writer(pred_fh, delimiter=',', quotechar='"')
 
-    # Write the header row.
-    pred_csv.writerow(['Id', 'Prediction'])
+#     # Write the header row.
+#     pred_csv.writerow(['Id', 'Prediction'])
 
-    pred_arr = build_svm(train_data2)
+#     pred_arr = build_svm(train_data2)
 
-    # for datum in test_data:
-    #     # pred_csv.writerow([datum['id'], mean_gap])
-    #     pred_csv.writerow([datum['id'], pred_avg(pred_arr, datum['features'])[0]])
+#     # for datum in test_data:
+#     #     # pred_csv.writerow([datum['id'], mean_gap])
+#     #     pred_csv.writerow([datum['id'], pred_avg(pred_arr, datum['features'])[0]])
 
-    rmse = 0
-    for datum in train_data:
-        rmse += (pred_avg(pred_arr, datum['features']) - datum['gap'])**2
+#     rmse = 0
+#     for datum in train_data:
+#         rmse += (pred_avg(pred_arr, datum['features']) - datum['gap'])**2
 
-    rmse = np.sqrt(rmse/samples)
-    print "LOL gg: ", rmse
+#     rmse = np.sqrt(rmse/samples)
+#     print "LOL gg: ", rmse
 
 
 
